16d2490a4d8cafd246b5d1060cb12761c647442a
guest6 <guest6@eco.co.kr>
2023-10-26 16:44:14 +0900

    update

--- 자동화/readme.md
+++ 자동화/readme.md
@@ -19,3 +19,6 @@ pip install -r requirements.txt
 
 ### 셀레니움
 - https://0433.tistory.com/41
+
+### 오디오 관련 라이브러리
+- [5종](https://richwind.co.kr/174)
---
+++ 자동화/특정 사이트 띄우기/index.py
@@ -0,0 +1,24 @@
+import webbrowser
+
+urls = [
+  "https://wikidocs.net/190",
+  "https://wikidocs.net/150542",
+  "https://www.startupcode.kr/company/blog/archives/17",
+  "https://richwind.co.kr/174",
+  "https://learngitbranching.js.org/?locale=ko",
+  "https://wikidocs.net/133239",
+  "https://velog.io/@pih6585/%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1%ED%95%98%EA%B8%B0",
+  "https://hongong.hanbit.co.kr/sql-%EA%B8%B0%EB%B3%B8-%EB%AC%B8%EB%B2%95-joininner-outer-cross-self-join/",
+  "https://leetcode.com/",
+  "https://www.tcpschool.com/xml/xml_xpath_intro",
+  "https://www.udemy.com/course/web-design-figma-webflow-freelancing/learn/lecture/31821968?show_review_popup=true#overview",
+  "https://www.udemy.com/courses/search/?src=ukw&q=canvas",
+  "https://www.inflearn.com/course/vue-%EC%99%84%EB%B2%BD-%EC%8B%A4%EC%A0%84/dashboard",
+  "https://shlee0882.tistory.com/110",
+  "https://github.com/CodingCitron",
+  "https://www.notion.so/"
+]
+
+# 아이디어 컴퓨터 관리자
+for url in urls:
+  webbrowser.open_new(url)
